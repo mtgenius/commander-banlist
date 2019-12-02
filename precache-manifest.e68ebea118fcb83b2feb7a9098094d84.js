009c6030630ed83a509bc3f16b5b3ffdfb1a8426
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-02 02:43:18 +0000

    Deploy mtgenius/commander-banlist to github.com/mtgenius/commander-banlist.git:gh-pages

---
+++ precache-manifest.e68ebea118fcb83b2feb7a9098094d84.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a58c2ae2c96a11ece0f224b08f5e644",
+    "url": "/commander-banlist/index.html"
+  },
+  {
+    "revision": "5eb3cf4813eb70aa9d49",
+    "url": "/commander-banlist/static/js/2.a2039778.chunk.js"
+  },
+  {
+    "revision": "9003188d4df243a928c0",
+    "url": "/commander-banlist/static/js/main.5110a49a.chunk.js"
+  },
+  {
+    "revision": "930eead68dbc14d567e1",
+    "url": "/commander-banlist/static/js/runtime-main.7921c825.js"
+  }
+]);
